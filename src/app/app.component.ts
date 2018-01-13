fc34912335fe484b7807de60b950c59acefe967a
Eduardo Magaldi <magaldi1989@gmail.com>
2018-01-13 19:22:48 -0200

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+	const title = 'Tour of Heroes';
+	const form = {};
+
+	changeName() {
+		console.log('changeName', form.name);
+	}
+
+	clickMessage = '';
+
+	onClickMe() {
+		this.clickMessage = 'You are my hero!';
+	}
+
+
+	modelChanged(newObj) {
+	    console.log('newObj', newObj);
+	}
 }
