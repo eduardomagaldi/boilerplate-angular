904d9545ae5ba596318b0e2329d3df3fcc8f70fc
Eduardo Magaldi <magaldi1989@gmail.com>
2018-01-14 16:32:32 -0200

    acessing form in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+// import {Component, ViewChild} from 'angular2/core';
+// ...
+
+// ...
+
 
 @Component({
 	selector: 'app-root',
@@ -6,8 +13,10 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	const title = 'Tour of Heroes';
-	const form = {};
+	title = 'Tour of Heroes';
+	form = {};
+
+	@ViewChild('myForm') myForm;
 
 	changeName() {
 		console.log('changeName', form.name);
@@ -19,6 +28,14 @@ export class AppComponent {
 		this.clickMessage = 'You are my hero!';
 	}
 
+	onSubmit() {
+		// console.log('onSubmit', myForm.form.valid);
+		this.showForm();
+	}
+
+	showForm() {
+	    console.log(this);
+	}
 
 	modelChanged(newObj) {
 	    console.log('newObj', newObj);
